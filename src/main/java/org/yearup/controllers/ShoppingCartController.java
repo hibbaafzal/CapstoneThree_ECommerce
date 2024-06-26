75f09404075ab83b7b465525371e071f59930715
hibbaafzal <166542360+hibbaafzal@users.noreply.github.com>
2024-06-26 12:52:44 -0400

    add to cart method, had bugs, fixed

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,3 +1,4 @@
+
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,23 +10,22 @@ import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
+import java.util.Map;
 
-
-// convert this class to a REST controller
-// only logged-in users should have access to these actions
 @RestController
 @RequestMapping("cart")
 @CrossOrigin
 @Secured({"ROLE_ADMIN", "ROLE_USER"})
-public class ShoppingCartController {
-
+public class ShoppingCartController
+{
     // a shopping cart requires
-    private ShoppingCartDao shoppingCartDao;
-    private UserDao userDao;
-    private ProductDao productDao;
+    private final ShoppingCartDao shoppingCartDao;
+    private final UserDao userDao;
+    private final ProductDao productDao;
 
     @Autowired
     public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
@@ -36,66 +36,89 @@ public class ShoppingCartController {
 
     // each method in this controller requires a Principal object as a parameter
     @GetMapping("")
-    public ShoppingCart getCart(Principal principal) {
-        try {
+    public ShoppingCart getCart(Principal principal)
+    {
+        try
+        {
             // get the currently logged-in username
             String userName = principal.getName();
-
             // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
             return shoppingCartDao.getByUserId(userId);
-        } catch (Exception e) {
+        }
+        catch(Exception e)
+        {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
     @PostMapping("products/{product_id}")
-    public void addItemToCart(Principal principal, @PathVariable int product_id) {
-        try {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ShoppingCart addItemToCart(Principal principal, @PathVariable int product_id) {
+        try
+        {
             // get the currently logged-in username
             String userName = principal.getName();
-
             // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
+            ShoppingCart shoppingCart = getCart(principal);
+
+            for (Map.Entry<Integer, ShoppingCartItem> item : shoppingCart.getItems().entrySet()) {
+                if (item.getValue().getProductId() == product_id) {
+                    shoppingCartDao.updateItemInCart(userId, product_id);
+                    return getCart(principal);
+                }
+            }
+
             shoppingCartDao.addItemToCart(userId, product_id);
-        } catch (Exception e) {
+
+        }
+        catch(Exception e)
+        {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
+        return getCart(principal);
     }
 
     @PutMapping("products/{product_id}")
     public int updateItemInCart(Principal principal, @PathVariable int product_id) {
-        try {
+        try
+        {
             // get the currently logged-in username
             String userName = principal.getName();
-
             // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
             return shoppingCartDao.updateItemInCart(userId, product_id);
-        } catch (Exception e) {
+        }
+        catch(Exception e)
+        {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
 
     @DeleteMapping("")
-    public int deleteCart(Principal principal) {
-        try {
+    public ShoppingCart deleteCart(Principal principal) {
+        try
+        {
             // get the currently logged-in username
             String userName = principal.getName();
-
             // find database user by userId
             User user = userDao.getByUserName(userName);
             int userId = user.getId();
 
-            return shoppingCartDao.deleteCart(userId);
-        } catch (Exception e) {
+            shoppingCartDao.deleteCart(userId);
+        }
+        catch(Exception e)
+        {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
+        return getCart(principal);
     }
+
 }
